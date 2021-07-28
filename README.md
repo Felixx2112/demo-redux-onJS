3ea5c3c1e6dfa8f4efe72923a8fdcc0aa0f39ab3
Felizz23 <thnhlong2112@gmail.com>
2021-07-28 12:57:59 +0700

    demo Redux

---
+++ README.md
@@ -0,0 +1,11 @@
+## code redux bang vanilla js
+
+- link bai viet(
+  Easy Frontend): https://www.youtube.com/watch?v=HOrA-uzZpw8&list=PLeS7aZkL6GOvCz3GiOtvtDXChJRuebb7S&index=3
+
+- Link cdn cua redux: https://unpkg.com/redux@4.1.0/dist/redux.js
+
+LAB: lam hobby list:
+
+- Render list of hobbies
+- Add form to add a new hobby
---
+++ main.js
@@ -0,0 +1,89 @@
+console.log(window.Redux);
+
+const { createStore } = window.Redux;
+
+//SETUP REDUX STORE
+// state
+// reducer
+// store
+
+//state
+// const initialState = ["Listen to music"];
+const initialState = JSON.parse(localStorage.getItem("hobby_list")) || [];
+
+//reducer
+const hobbyReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "ADD_HOBBY": {
+      const newList = [...state];
+      newList.push(action.payload);
+
+      return newList;
+    }
+    default:
+      return state;
+  }
+};
+
+//store
+const store = createStore(hobbyReducer);
+
+//------------------------
+
+//RENDER REDUX HOBBY LIST
+
+const renderHobbyList = (hobbyList) => {
+  if (!Array.isArray(hobbyList) || hobbyList.length === 0) return;
+
+  const ulElement = document.querySelector("#hobbyListId");
+  if (!ulElement) return;
+
+  //reset previous content of ul
+  ulElement.innerHTML = "";
+
+  //loop
+  for (const hobby of hobbyList) {
+    const liElement = document.createElement("li");
+    liElement.textContent = hobby;
+
+    ulElement.appendChild(liElement);
+  }
+};
+
+//RENDER INITIAL HOBBY LIST
+const initialHobbyList = store.getState();
+console.log(initialHobbyList);
+renderHobbyList(initialHobbyList);
+
+//HANDLE FORM SUBMIT
+const hobbyFormElement = document.querySelector("#hobbyForm");
+if (hobbyFormElement) {
+  const handleSubmit = (e) => {
+    //prevent browser from reloading
+    e.preventDefault();
+
+    const hobbyTextElement = hobbyFormElement.querySelector("#hobbyTextId");
+    if (!hobbyTextElement) return;
+    console.log("hobbyText: ", hobbyTextElement.value);
+
+    //create action
+    const action = {
+      type: "ADD_HOBBY",
+      payload: hobbyTextElement.value,
+    };
+    //dispatch action
+    store.dispatch(action);
+
+    //reset form
+    hobbyFormElement.reset();
+  };
+  hobbyFormElement.addEventListener("submit", handleSubmit);
+}
+
+store.subscribe(() => {
+  console.log("STATE UPDATE: ", store.getState());
+  const newHobbyList = store.getState();
+  renderHobbyList(newHobbyList);
+
+  localStorage.setItem("hobby_list", JSON.stringify(newHobbyList));
+});
